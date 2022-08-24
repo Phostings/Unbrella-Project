c9828ce5a8e97188e29f167566b579e0e865d03d
Phostings <Summerhannah1@gmail.com>
2022-08-24 16:55:22 -0400

    get data from database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
+const UserModel = require('./models/Users')
 
 mongoose.connect("mongodb+srv://phostings:parasite1@cluster0.7uvug1v.mongodb.net/newdatabase?retryWrites=true&w=majority"
 );
+
+app.get("/getUsers", (req, res) => {
+    UserModel.find({}, (err, result) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
 
 app.listen(3001, () => {
     console.log("server is running");
