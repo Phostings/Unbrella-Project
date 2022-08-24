f6e3cfa1d9ce4402a4bd692875a38b5fbc3ebe82
Phostings <Summerhannah1@gmail.com>
2022-08-24 16:38:59 -0400

    connect database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
 
+mongoose.connect("mongodb+srv://phostings:parasite1@cluster0.7uvug1v.mongodb.net/newdatabase?retryWrites=true&w=majority"
+);
 
 app.listen(3001, () => {
     console.log("server is running");
