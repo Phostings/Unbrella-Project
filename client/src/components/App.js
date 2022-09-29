c4a2ebf9b941fd6406ad7d054832a86eeb9e9a7e
Phostings <Summerhannah1@gmail.com>
2022-09-29 16:59:18 -0400

    auth completed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,6 +4,10 @@ import { Container } from 'react-bootstrap';
 import AuthProvider from '../contexts/AuthContext';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Dashboard from "./Dashboard"
+import Login from './Login'
+import PrivateRoute from './PrivateRoute';
+import ForgotPassword from './ForgotPassword';
+import UpdateProfile from './UpdateProfile';
 
 
 function App() {
@@ -16,8 +20,17 @@ function App() {
           <Router>
             <AuthProvider>
               <Routes>
-                <Route exact path="/" element={<Dashboard/>} />
+                <Route path="/" element={<PrivateRoute>
+                                        <Dashboard/>
+                                        </PrivateRoute>}
+                                        ></Route>
+                <Route path="/update-profile" element={<PrivateRoute>
+                                        <UpdateProfile/>
+                                        </PrivateRoute>}
+                                        ></Route>
                 <Route path="/signup" element={<Signup/>} />
+                <Route path="/login"  element={<Login/>} />
+                <Route path="/forgot-password" element={<ForgotPassword/>} />
               </Routes>
             </AuthProvider>
           </Router>
